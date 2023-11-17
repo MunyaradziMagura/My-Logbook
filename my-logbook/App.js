30711764da8a5e8183e2b6128433dd651f833ac8
Munyaradzi Magura <munyaradziMagura@outlook.com>
2023-11-17 18:21:57 +1030

    implemented react paper UI library and tested it with a basic press button

--- my-logbook/App.js
+++ my-logbook/App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as React from 'react';
+import { Button } from 'react-native-paper';
+import { useState } from 'react';
 export default function App() {
+ const [text, setText] = useState(1)
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>{text}</Text>
+      <Button icon="camera" mode="contained" onPress={() => setText(text + 1)}>
+    Press me
+  </Button>
       <StatusBar style="auto" />
     </View>
   );
