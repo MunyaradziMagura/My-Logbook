93fe84b458ec0efa22b14ecc7ddeda9609bf336e
Munyaradzi Magura <munyaradziMagura@outlook.com>
2023-11-17 18:43:42 +1030

    added typescript and created pages

--- my-logbook/App.tsx
+++ my-logbook/App.tsx
@@ -1,16 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
-import { Button } from 'react-native-paper';
-import { useState } from 'react';
 export default function App() {
- const [text, setText] = useState(1)
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button icon="camera" mode="contained" onPress={() => setText(text + 1)}>
-    Press me
-  </Button>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
