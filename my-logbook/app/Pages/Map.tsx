84e03d54999f3800179a9249f6035f4ce348abfc
Munyaradzi Magura <munyaradziMagura@outlook.com>
2023-11-19 20:55:32 +1030

    added css to change the colour of the buttons on the map view

--- my-logbook/app/Pages/Map.tsx
+++ my-logbook/app/Pages/Map.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View,Text, SafeAreaView } from 'react-native';
 import MapView from 'react-native-maps'
 import { Card, Button } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
+import { BlurView } from 'expo-blur';
 
 
 export default function Map() {
+  // this object controls all the values for the color of the background of the maps start and stop buttons
+  const [getClickedGradient, setClickedGradient] = useState(['#090979','#00d4ff'])
+
+
   return (
     <>
-      
-
         <MapView style={styles.map} 
     initialRegion={{
     latitude: -34.9263,
@@ -18,16 +22,33 @@ export default function Map() {
   }}/>
    <SafeAreaView>
         <Card>
+        <LinearGradient
+        // Background Linear Gradient
+        colors={getClickedGradient}
+        start={{ x: 0, y: 0.5 }} // Left
+        end={{ x: 1, y: 0.5 }}   // Right
+        style={styles.background}
+      />
+
           <View style={styles.cardActions}>
-            <Button style={styles.startButton}>Start</Button>
-            <Button style={styles.stopButton}>Stop</Button>
+          <BlurView intensity={100} style={styles.StartBlurContainer}>
+          <Button  onPress={() => {
+              setClickedGradient(['rgba(14,174,87,1)','rgba(12,116,117,1)'])
+            }}><Text style={styles.text}>GO</Text></Button>
+        </BlurView>
+        <BlurView intensity={100} style={styles.StopBlurContainer}>
+
+        <Button onPress={() => {
+              setClickedGradient(['#fe6603','#f42f4a'])
+            }}><Text style={styles.text} >Stop</Text></Button>
+        </BlurView>
+
           </View>
         </Card>
       </SafeAreaView>
     </>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -40,9 +61,35 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 16, // Adjust the padding as needed
+    backgroundColor: 'transparent'
   },
-  stopButton: {
-    // alignSelf: 'flex-end', // No longer needed
+  StartBlurContainer:{
+    flex: 1,
+    textAlign: 'left',
+    justifyContent: 'flex-start',
+    overflow: 'hidden',
+    borderRadius: 20,
+    },
+    StopBlurContainer:{
+      flex: 1,
+      textAlign: 'right',
+      justifyContent: 'flex-end',
+      marginLeft:20,
+      overflow: 'hidden',
+      borderRadius: 20,
+      },
+  
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
   },
-  startButton: {},
+  text:{
+    paddingTop: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff'
+  }
 });
