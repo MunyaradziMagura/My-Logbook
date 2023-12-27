be58543413b6feffc084cc4406b1ab59a78617c5
Munya <munyaradziMagura@outlook.com>
2023-12-27 20:24:32 +1100

    Update Profile.tsx

--- my-logbook/app/Pages/Profile.tsx
+++ my-logbook/app/Pages/Profile.tsx
@@ -1,40 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import React from 'react'
+import { View, Text } from 'react-native'
 
-import * as Location from 'expo-location';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  (async () => {
-
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('Permission to access location was denied');
-      return (
-        <SafeAreaView>
-          <Text style={{ width: '100%', height: '100%', justifyContent: 'center', alignContent: 'center', textAlign: 'center' }}> ERROR FINDING LOCATION -- Permission to access location was denied </Text>
-        </SafeAreaView>
-      )
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    setLocation(location);
-  })();
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-    console.log(text)
-  }
-
+export default function Profile() {
   return (
-    <View >
-      <Text>{text}</Text>
-    </View>
-  );
+    <View><Text>Profile</Text></View>
+  )
 }
+
