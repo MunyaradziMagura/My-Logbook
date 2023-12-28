3615687d93add83d0a731fa94fa0b1059ba269ef
Munya <munyaradziMagura@outlook.com>
2023-12-28 12:53:52 +1030

    fix issue on profile page where content would not show

--- my-logbook/App.tsx
+++ my-logbook/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { SafeAreaView} from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { BottomNavigation } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 // import pages 
@@ -9,23 +9,23 @@ import Profile from './app/Pages/Profile';
 import SupervisorSignature from './app/Pages/SupervisorSignature';
 
 // fetch pages
-const MapRoute = () => <Map/>;
+const MapRoute = () => <Map />;
 
-const ProfileRoute = () => <SafeAreaView><Profile/></SafeAreaView>;
+const ProfileRoute = () => <Profile />;
 
-const SupervisorRoute = () => <SafeAreaView><SupervisorSignature/></SafeAreaView>;
+const SupervisorRoute = () => <SafeAreaView><SupervisorSignature /></SafeAreaView>;
 
-const ExportRoute = () => <SafeAreaView><Export/></SafeAreaView>;
+const ExportRoute = () => <SafeAreaView><Export /></SafeAreaView>;
 
 // run application
 export default function App() {
 
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'Map', title: 'Map', focusedIcon: 'navigation', unfocusedIcon: 'navigation-outline'},
-    { key: 'Profile', title: 'Profile',  focusedIcon: 'account', unfocusedIcon: 'account-outline' },
-    { key: 'Supervisor', title: 'Supervisor',  focusedIcon: 'account-eye', unfocusedIcon: 'account-eye-outline' },
-    { key: 'Export', title: 'Export',  focusedIcon: 'download', unfocusedIcon: 'download-outline'},
+    { key: 'Map', title: 'Map', focusedIcon: 'navigation', unfocusedIcon: 'navigation-outline' },
+    { key: 'Profile', title: 'Profile', focusedIcon: 'account', unfocusedIcon: 'account-outline' },
+    { key: 'Supervisor', title: 'Supervisor', focusedIcon: 'account-eye', unfocusedIcon: 'account-eye-outline' },
+    { key: 'Export', title: 'Export', focusedIcon: 'download', unfocusedIcon: 'download-outline' },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
@@ -36,13 +36,13 @@ export default function App() {
   });
   return (
     <SafeAreaProvider>
-       <BottomNavigation
-      navigationState={{ index, routes }}
-      onIndexChange={setIndex}
-      renderScene={renderScene}
-      inactiveColor={'grey'}
-      shifting={true}
-    />
+      <BottomNavigation
+        navigationState={{ index, routes }}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+        inactiveColor={'grey'}
+        shifting={true}
+      />
     </SafeAreaProvider>
   );
 }
--- my-logbook/app/Pages/Profile.tsx
+++ my-logbook/app/Pages/Profile.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
 
 export default function Profile() {
   return (
-    <View><Text>Profile</Text></View>
-  )
+    <SafeAreaView
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={{ flex: 1, backgroundColor: 'red' }} />
+      <View style={{ flex: 2, backgroundColor: 'darkorange' }} />
+      <View style={{ flex: 3, backgroundColor: 'green' }} />
+    </SafeAreaView>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
 
