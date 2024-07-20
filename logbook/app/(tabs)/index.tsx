8df5e202f50838aacbc26ce873576a648fda7d80
MunyaradziMagura <MunyaradziMagura@outlook.com>
2024-07-20 02:33:44 -0400

    added a navigation with custom menu items

---
+++ logbook/app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>This is where we are</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  });
